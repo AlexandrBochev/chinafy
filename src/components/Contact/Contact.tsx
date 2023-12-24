dc2183a25f1862efebe4b39c22fcd064e27981d2
Alexandr Bochev <abochev@gmail.com>
2023-12-25 00:38:02 +0200

    Contact share

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,13 +1,22 @@
 import background from '../../assets/contact/contact-bg.jpg'
+import share from '../../assets/contact/share.svg'
+import face from '../../assets/contact/face.jpg'
 import { Input } from './Input/Input'
 
 const Contact = () => {
   return (
     <section className="relative pt-[3.125rem] pb-[5.625rem] md:pt-[6.25rem]" id='Contact'>
       <img src={ background } alt="Background" className="absolute top-0 left-0 w-full h-full object-cover -z-30" />
-      <div className="container mx-auto">
+      <div className="relative container mx-auto">
 
-        {/* Title */}
+        {/* Heading */}
+        <div className="absolute top-0 hidden xl:flex items-start text-left text-[1.25rem]">
+          <img src={ share } alt="Share Icon" />
+          <div className="flex items-center">
+            <img src={ face } alt="Face Icon" className="rounded-full mx-3" />
+            <p>Have a<br />question?</p>
+          </div>
+        </div>
         <div className="w-[16.6rem] md:w-[40rem] mb-10 md:mb-12 mx-auto">
           <div className="relative mb-8 mx-auto">
             <span className="absolute bottom-0 right-7 h-9 w-32 md:h-16 md:w-[10.4rem] md:right-[13rem] bg-lilac rounded-lg -z-10" />
--- src/components/Contact/Input/Input.tsx
+++ src/components/Contact/Input/Input.tsx
@@ -6,10 +6,12 @@ interface InputProps {
 
 const Input = ({ type, label, placeholder }: InputProps) => {
   return (
-    <div className="mb-6 lg:mb-10">
+    <div className="flex flex-wrap mb-6 lg:mb-10">
       <label htmlFor="name" className="text-2xl lg:text-4xl font-medium mr-4">{ label }</label>
-      <input type={ type } name="name" placeholder={ placeholder } className="min-w-[14.2rem] border-b border-gray placeholder:text-black lg:placeholder:text-xl pb-[1px]" required />
-      <label htmlFor="name" className="text-2xl lg:text-4xl font-medium ml-4">.</label>
+      <div className="flex items-end">
+        <input type={ type } name="name" placeholder={ placeholder } className="min-w-[14.2rem] border-b border-gray placeholder:text-black lg:placeholder:text-xl pb-[1px]" required />
+        <label htmlFor="name" className="text-2xl lg:text-4xl font-medium ml-4">.</label>
+      </div>
     </div>
   )
 }
