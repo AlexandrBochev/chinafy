87b982ac578351a1d2ff836af766a287031afa9d
Alexandr Bochev <abochev@gmail.com>
2023-12-28 15:18:44 +0200

    SwitchBtn

--- src/components/Plans/SwitchBtn/SwitchBtn.tsx
+++ src/components/Plans/SwitchBtn/SwitchBtn.tsx
@@ -12,7 +12,7 @@ const SwitchBtn = ({ onClick, isAnnual, title }: SwitchBtnProps) => {
     >
       <div
         className={`
-          rounded-full bg-lilac transition-all
+          rounded-full bg-lilac transition-all duration-300
           ${ isAnnual ? 'w-[1.875rem] lg:w-9 h-[1.875rem] lg:h-9 ml-[0.1875rem] lg:ml-[0.375rem] mr-2' : 'w-0 h-0 mr-5 lg:mr-6' }
         `}
       />
