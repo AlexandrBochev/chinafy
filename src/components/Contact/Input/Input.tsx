22913c54dacdc17927047c87da3a35df74bce200
Alexandr Bochev <abochev@gmail.com>
2023-12-28 18:11:52 +0200

    matter

--- src/components/Contact/Input/Input.tsx
+++ src/components/Contact/Input/Input.tsx
@@ -9,7 +9,7 @@ const Input = ({ type, label, placeholder }: InputProps) => {
     <div className="flex flex-wrap mb-6 lg:mb-10">
       <label htmlFor="name" className="text-2xl lg:text-4xl font-medium mr-4">{ label }</label>
       <div className="flex items-end">
-        <input type={ type } name="name" placeholder={ placeholder } className="min-w-[14.2rem] border-b border-gray placeholder:text-black lg:placeholder:text-xl pb-[1px]" required />
+        <input type={ type } name="name" placeholder={ placeholder } className="min-w-[14.2rem] border-b border-gray placeholder:text-black lg:placeholder:text-xl pb-[1px] outline-none" required />
         <label htmlFor="name" className="text-2xl lg:text-4xl font-medium ml-4">.</label>
       </div>
     </div>
